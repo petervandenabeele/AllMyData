060247569a2381ea1b996e40d5bdbe523f9d3066
Peter Vandenabeele <peter@vandenabeele.com>
2014-12-02 17:59:44 +0100

    Add empty FactEncoder class

---
+++ src/main/scala/encoding/FactEncoder.scala
@@ -0,0 +1,8 @@
+/**
+ * Created by peter_v on 02/12/14.
+ */
+
+package encoding
+
+class FactEncoder {
+}
---
+++ src/test/scala/encoding/FactEncoderSuite.scala
@@ -0,0 +1,16 @@
+/**
+ * Created by peter_v on 02/12/14.
+ */
+
+package encoding
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class FactEncoderSuite extends FunSuite {
+  test("new FactEncoder() works") {
+    new FactEncoder()
+  }
+}
