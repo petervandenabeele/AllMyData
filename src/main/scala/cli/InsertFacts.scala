22eaf7a372569fb323d44c7d16da778eb693ceeb
Peter Vandenabeele <peter@vandenabeele.com>
2014-12-05 08:29:08 +0100

    Object InstertFact exists

---
+++ src/main/scala/cli/InsertFacts.scala
@@ -0,0 +1,11 @@
+/**
+ * Created by peter_v on 04/12/14.
+ */
+
+package cli
+
+object InsertFacts {
+  def main(args: Array[String]): Unit = {
+    println("Starting AllMyData InsertFacts.main")
+  }
+}
---
+++ src/test/scala/cli/InsertFactsSuite.scala
@@ -0,0 +1,17 @@
+/**
+ * Created by peter_v on 04/12/14.
+ */
+
+package cli
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class InsertFactsSuite extends FunSuite {
+
+  test("Object InstertFact exists") {
+    val insterter = InsertFacts
+  }
+}
