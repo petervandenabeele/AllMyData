28fd3413be33658fdf95dac69188fbc650f5a184
Peter Vandenabeele <peter@vandenabeele.com>
2014-12-08 00:15:02 +0100

    Using local_id, one_fact.csv creates 3 facts for one resource
    * imperfect implementation ... (too long functions, uses a var
      for the Map that holds the local_id -> ATD_Uuid mappings)

--- src/main/scala/cli/FactsInserter.scala
+++ src/main/scala/cli/FactsInserter.scala
@@ -5,6 +5,7 @@
 package cli
 
 import base.Fact
+import common._
 import scala.io.BufferedSource
 
 object FactsInserter {
@@ -13,17 +14,55 @@ object FactsInserter {
   }
 
   // reading from a CSV with structure
-  // optional local id | optional subject | predicate | objestType | object Value
+  // optional local_id | optional subject | predicate | objestType | object Value
   def reader(file: BufferedSource): Iterator[Fact] = {
-    file.getLines().map[Fact](line => factFrom_CSV_Line(line))
+    var uuids = scala.collection.mutable.Map[Int, ATD_Uuid]()
+
+    file.getLines().map[Fact](line => {
+      val elements:Array[String] = line.split(",", 5)
+      val local_id_string = elements(0)
+      val predicate = elements(2)
+      val objectType = elements(3)
+      val objectValue = elements(4)
+
+      val local_id: Option[Int] = local_id_string match {
+        case "" => None
+        case _ => Some(local_id_string.toInt)
+      }
+      val uuidOption = local_id match {
+        case None => None
+        case Some(i) => uuids.get(i)
+      }
+      val fact = factFrom_CSV_Line(
+        predicate = predicate,
+        objectType = objectType,
+        objectValue = objectValue,
+        uuidOption)
+      if (uuidOption.isEmpty && !local_id.isEmpty) {
+        uuids += (local_id.get -> fact.uuid)
+      }
+      fact
+    })
   }
 
-  def factFrom_CSV_Line(line: String) = {
-    val elements:Array[String] = line.split(",", 5)
-    Fact(
-      predicate = elements(2),
-      objectType = elements(3),
-      objectValue = elements(4)
-    )
+  def factFrom_CSV_Line(predicate: ATD_Predicate,
+                        objectType: ATD_ObjectType,
+                        objectValue: ATD_ObjectValue,
+                        uuidOption: Option[ATD_Uuid]) = {
+    uuidOption match {
+      case Some(uuid) =>
+        Fact(
+          uuid = uuid,
+          predicate = predicate,
+          objectType = objectType,
+          objectValue = objectValue
+        )
+      case None =>
+        Fact(
+          predicate = predicate,
+          objectType = objectType,
+          objectValue = objectValue
+        )
+    }
   }
 }
--- src/test/scala/cli/FactsInserterSuite.scala
+++ src/test/scala/cli/FactsInserterSuite.scala
@@ -35,4 +35,32 @@ class FactsInserterSuite extends FunSuite {
     assert(fact.objectType === "s")
     assert(fact.objectValue === "bar")
   }
+
+  test("one_fact.csv creates 3 facts for one resource") {
+    val filename = "/one_fact.csv"
+    val file = scala.io.Source.fromURL(getClass.getResource(filename))
+    val factIterator = reader(file)
+    val facts: Array[Fact] = factIterator.toArray
+    val fact_1 = facts(0)
+    val fact_2 = facts(1)
+    val fact_3 = facts(2)
+
+    assert(facts.size === 3)
+
+    assert(fact_1.predicate === "atd:foo")
+    assert(fact_1.objectType === "s")
+    assert(fact_1.objectValue === "bar")
+
+    assert(fact_2.predicate === "atd:tux")
+    assert(fact_2.objectType === "s")
+    assert(fact_2.objectValue === "ping")
+
+    assert(fact_3.predicate === "atd:ping")
+    assert(fact_3.objectType === "s")
+    assert(fact_3.objectValue === "pong")
+
+    val resource_uuid = fact_1.uuid
+    assert(fact_2.uuid === resource_uuid)
+    assert(fact_3.uuid === resource_uuid)
+  }
 }
