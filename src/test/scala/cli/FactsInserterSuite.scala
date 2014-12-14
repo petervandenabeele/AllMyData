6d6f7ce8d1b6af74dc406d32773d7bfb2911b5ba
Peter Vandenabeele <peter@vandenabeele.com>
2014-12-14 19:08:37 +0100

    2 facts with 2 different contexts

--- src/test/scala/cli/FactsInserterSuite.scala
+++ src/test/scala/cli/FactsInserterSuite.scala
@@ -108,52 +108,30 @@ class FactsInserterSuite extends FunSuite {
     assert(fact_3.subject === resource_subject)
   }
 
-  test("two_facts.csv creates 2 resources with 3 facts each") {
-    val filename = "/two_facts.csv"
+  test("two_facts_with_context.csv 2 contexts for 2 facts") {
+    val filename = "/two_facts_with_context.csv"
     val file = scala.io.Source.fromURL(getClass.getResource(filename))
     val factIterator = reader(file)
     val facts: Array[Fact] = factIterator.toArray
 
-    // scrambled order (but it still works :-)
-    val fact_3 = facts(4)
-    val fact_1 = facts(0)
-    val fact_2 = facts(2)
-    val fact_4 = facts(1)
-    val fact_5 = facts(3)
-    val fact_6 = facts(5)
+    val context_1 = facts(0)
+    val context_2 = facts(1)
+    val fact_1 = facts(2)
+    val fact_2 = facts(3)
 
-    assert(facts.size === 6)
+    assert(facts.size === 4)
 
+    val context_subject_1 = context_1.subject
+    val context_subject_2 = context_2.subject
+
+    assert(fact_1.context === context_subject_1)
     assert(fact_1.predicate === "atd:foo")
     assert(fact_1.objectType === "s")
     assert(fact_1.objectValue === "bar")
 
-    assert(fact_2.predicate === "atd:tux")
+    assert(fact_2.context === context_subject_2)
+    assert(fact_2.predicate === "atd:bar")
     assert(fact_2.objectType === "s")
-    assert(fact_2.objectValue === "ping")
-
-    assert(fact_3.predicate === "atd:ping")
-    assert(fact_3.objectType === "s")
-    assert(fact_3.objectValue === "pong")
-
-    assert(fact_4.predicate === "atd:bar")
-    assert(fact_4.objectType === "s")
-    assert(fact_4.objectValue === "foo")
-
-    assert(fact_5.predicate === "atd:ping")
-    assert(fact_5.objectType === "s")
-    assert(fact_5.objectValue === "tux")
-
-    assert(fact_6.predicate === "atd:pong")
-    assert(fact_6.objectType === "s")
-    assert(fact_6.objectValue === "ping")
-
-    val resource_subject_1 = fact_1.subject
-    assert(fact_2.subject === resource_subject_1)
-    assert(fact_3.subject === resource_subject_1)
-
-    val resource_subject_2 = fact_4.subject
-    assert(fact_5.subject === resource_subject_2)
-    assert(fact_6.subject === resource_subject_2)
+    assert(fact_2.objectValue === "foo")
   }
 }
