4217f4633bc4e8773f91dca3ba13baa3ad50f28c
Peter Vandenabeele <peter@vandenabeele.com>
2015-03-08 12:41:27 +0100

    Wait a bit longer for zookeeper and server

--- src/scripts/start-kafka.sh
+++ src/scripts/start-kafka.sh
@@ -12,13 +12,13 @@ echo "Starting Zookeeper"
 cat config/zookeeper.properties
 bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
 
-sleep 4
+sleep 8
 
 echo "Starting Kafka"
 cat config/server.properties
 bin/kafka-server-start.sh -daemon config/server.properties
 
-sleep 4
+sleep 8
 
 echo "Starting consumer on ATD_test"
 bin/kafka-console-consumer.sh --zookeeper trusty64:2181 --topic ATD_test --from-beginning
