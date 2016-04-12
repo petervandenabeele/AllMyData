32ce2bb28d144b5f10c70af7aaba2a146f3ebc8e
Peter Vandenabeele <peter@vandenabeele.com>
2016-04-12 09:26:41 +0200

    Clean-up Fact class
    * make now and today parsing more robust
    * place "local" vals for validPredicates in block
    * reformat code
    * improve documentation

--- src/main/scala/base/Fact.scala
+++ src/main/scala/base/Fact.scala
@@ -10,17 +10,17 @@ import java.time.{ZoneId, ZonedDateTime}
 import cli.Util
 import common._
 
-/** The core abstraction of this data model.
+/** The core abstraction of this data model. The full log of all information is stored as
+  * an ordered Sequence of facts. Each fact has:
   *
-  * The full log of all information is stored as an ordered Sequence of facts.
-  *
-  * Each fact has id, timestamp, context, subject, predicate, objectType, objectValue, and
+  * id, timestamp, context, subject, predicate, objectType, objectValue, and
   * optional `at`, `from`, `to` timestamps (`from` and `to` are inclusive)
   *
   * Construction can only occur from a predicateObject (that is the real "data" part with validations).
+  *
+  * TODO Fix the timestamp to have more digits and/or be monotonic.
   */
 
-// TODO Fix the timestamp to have more digits and/or be monotonic
 case class Fact(timestamp: AMD_Timestamp = Fact.now,
                 id: AMD_Id = newUUID,
                 context: Context = Context(None),
@@ -28,11 +28,16 @@ case class Fact(timestamp: AMD_Timestamp = Fact.now,
                 predicateObject: PredicateObject) {
 
   def predicate = predicateObject.predicate
+
   def objectType = predicateObject.objectType
+
   def objectValue = predicateObject.objectValue
-  def at   = predicateObject.at
+
+  def at = predicateObject.at
+
   def from = predicateObject.from
-  def to   = predicateObject.to
+
+  def to = predicateObject.to
 
   override def toString: String = {
     List(
@@ -48,16 +53,21 @@ case class Fact(timestamp: AMD_Timestamp = Fact.now,
 }
 
 object Fact {
-  def now = ZonedDateTime.now(ZoneId.of("UTC")).toString.dropRight(5) // drop `[UTC]`
-  def today = now.dropRight(14) // drop the "THH:MM:SS.mmmZ"
-  private val resourceName = "/predicates/valid_predicates.csv"
-  private val resourceFile = getClass.getResource(resourceName)
-  private val file =
-    if (resourceFile.isInstanceOf[URL])
-      scala.io.Source.fromURL(resourceFile)
-    else
-      scala.io.Source.fromFile(Util.getFullFilename("valid_predicates.csv", "metadata"))
+  /** drop "[UTC]" */
+  def now = ZonedDateTime.now(ZoneId.of("UTC")).toString.takeWhile(c => c != '[')
 
-  val validPredicates: Set[String] =
+  /** drop the "THH:MM:SS.mmmZ" part */
+  def today = now.takeWhile(c => c != 'T')
+
+  val validPredicates: Set[String] = {
+    val resourceName = "/predicates/valid_predicates.csv"
+    val resourceFile = getClass.getResource(resourceName)
+    val file =
+      if (resourceFile.isInstanceOf[URL])
+        scala.io.Source.fromURL(resourceFile)
+      else
+        scala.io.Source.fromFile(Util.getFullFilename("valid_predicates.csv", "metadata"))
+
     file.getLines().filterNot(x => x.isEmpty).map(line => line.split(separator, 2)(0)).toSet
+  }
 }
